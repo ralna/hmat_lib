ecf864e1fb6b5615773ff4490f029488cf393b56
Rastislav Turanyi <rastislav.turanyi@stfc.ac.uk>
2025-03-19 09:42:31 +0000

    Test out that using a subset of matrix for SVD doesn't cause issues

--- svd.c
+++ svd.c
@@ -2,16 +2,41 @@
 #include <stdlib.h>
 
 int main() {
-  int m = 4, n = 3;
+  int m = 10, n = 10;
   int n_singular_values = m < n ? m : n;
+  int idx;
 
   double *matrix = malloc(m * n * sizeof(double));
-  matrix[0] = 5;
-  matrix[1] = 0;
-  matrix[2] = 1;
-  matrix[3] = 0; matrix[4] = -5; matrix[5] = 0;
-  matrix[6] = 1; matrix[7] = 0; matrix[8] = 1;
-  matrix[9] = 1; matrix[10] = 1; matrix[11] = 1;
+  //matrix[0] = 5; matrix[1] = 0; matrix[2] = 1;
+  //matrix[3] = 0; matrix[4] = -5; matrix[5] = 0;
+  //matrix[6] = 1; matrix[7] = 0; matrix[8] = 1;
+  //matrix[9] = 1; matrix[10] = 1; matrix[11] = 1;
+  for (int i = 0; i<m; i++) {
+    for (int j = 0; j<m; j++) {
+      idx = j + i * m;
+      if (i == j) {
+        matrix[idx] = 1;
+      } else if (i == j+1 || i == j-1) {
+        matrix[idx] = 0.5;
+      } else {
+        matrix[idx] = 0;
+      }
+    }
+  }
+
+  m = 5; n = 5; n_singular_values = 5;
+  for (int i = 0; i<m; i++) {
+    for (int j = 0; j<m; j++) {
+      idx = j + i * m;
+      if (i == j) {
+        matrix[idx] = 1;
+      } else if (i == j+1 || i == j-1) {
+        matrix[idx] = 0.5;
+      } else {
+        matrix[idx] = 1;
+      }
+    }
+  }
 
   double *s = malloc(n_singular_values * sizeof(double));
   double *u = malloc(m * n_singular_values * sizeof(double));
@@ -46,6 +71,7 @@ int main() {
     return 1;
   }
 
+  printf("U:\n");
   for (int i=0; i<n; i++) {
     for (int j=0; j<n_singular_values; j++) {
       printf("%f    ", u[i + j * m]);
@@ -54,11 +80,13 @@ int main() {
   }
   printf("\n");
 
+  printf("S:\n");
   for (int i=0; i<n_singular_values; i++) {
     printf("%f    ", s[i]);
   }
   printf("\n");
 
+  printf("VT:\n");
   for (int i=0; i<n; i++) {
     for (int j=0; j<n_singular_values; j++) {
       printf("%f    ", vt[i + j * m]);
