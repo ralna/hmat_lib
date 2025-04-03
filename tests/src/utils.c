ed678550ce5bbd4e588e6b287dbef56211ace80f
Rastislav Turanyi <rastislav.turanyi@stfc.ac.uk>
2025-04-03 15:20:05 +0100

    Update error handling
    Updates error handling for dense_to_tree_hodlr, adds a BLAS wrapper, and removes og outer product

--- tests/src/utils.c
+++ tests/src/utils.c
@@ -1,52 +1,47 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <string.h>
 
-#include <criterion/logging.h>
 #include <criterion/criterion.h>
+#include <criterion/logging.h>
 #include <criterion/new/assert.h>
 
 #include "../include/utils.h"
 
-
 static double DELTA = 1e-10;
 
 
-void log_matrix(double *matrix, int m, int n, int lda) {
-  for (int i=0; i<m; i++) {
-    for (int j=0; j < n; j++) {
-      printf("%f    ", matrix[j * lda + i]);
+void log_matrix(const double *matrix, const int m, const int n, const int lda) {
+  for (int i = 0; i < m; i++) {
+    char *buffer = malloc(n * 16 * sizeof(char));
+    buffer[0] = '\0';
+
+    for (int j = 0; j < n; j++) {
+      char temp[16];
+      snprintf(temp, sizeof(temp), "%12.5e  ", matrix[j * lda + i]);
+      strcat(buffer, temp);
+      //printf("%f    ", matrix[j * lda + i]);
     }
-    printf("\n");
+    //printf("\n");
+    cr_log_info("%s", buffer);
+    free(buffer);
   }
-  printf("\n");
+  //printf("\n");
 }
 
 
-struct ParametersTestCompress {
-  int m_full;
-  int m;
-  int n;
-  double svd_threshold;
-  int expected_n_singular;
-  double *matrix;
-  double *u_expected;
-  double *v_expected;
-  double *full_matrix;
-};
-
-
-void expect_arr_double_eq(double *actual, 
-                          double *expected, 
-                          int m, 
-                          int n,
-                          int ld_actual,
-                          int ld_expected) {
+void expect_arr_double_eq(const double *restrict actual, const double *restrict expected, 
+                          const int m, const int n,
+                          const int ld_actual, const int ld_expected) {
   int errors = 0;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
-      if (fabs(actual[j + i * ld_actual] - expected[j + i * ld_expected]) > DELTA) {
-        cr_log_error("actual value '%f' at index [%d, %d] is different from the expected '%f'", 
-                     actual[j + i * ld_actual], j, i, expected[j + i * ld_expected]);
+      if (fabs(actual[j + i * ld_actual] - expected[j + i * ld_expected]) >
+          DELTA) {
+        cr_log_error("actual value '%f' at index [%d, %d] is different from "
+                     "the expected '%f'",
+                     actual[j + i * ld_actual], j, i,
+                     expected[j + i * ld_expected]);
         errors += 1;
       }
     }
@@ -60,6 +55,4 @@ void expect_arr_double_eq(double *actual,
     log_matrix(expected, m, n, ld_expected);
   }
 }
-
-
 
