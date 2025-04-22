14e291b924a79c4ab6aaca379d94f72c93b2a97d
Rastislav Turanyi <rastislav.turanyi@stfc.ac.uk>
2025-04-22 16:44:34 +0100

    Test multiple heights for vector mult

--- tests/src/test_tree_algebra.c
+++ tests/src/test_tree_algebra.c
@@ -34,39 +34,46 @@ void free_hv_params(struct criterion_test_params *params) {
 
 
 struct ParametersTestHxV * generate_compress_params(int * len) {
-  int n_params = 2, ierr = 0;
+  int n_params = 6, ierr = 0;
   *len = n_params;
   struct ParametersTestHxV *params = cr_malloc(n_params * sizeof(struct ParametersTestHxV));
   double svd_threshold = 0.1;
-  params[0].len = 21;
-  params[0].hodlr = allocate_tree_cr(1, &ierr);
-  dense_to_tree_hodlr_cr(params[0].hodlr, params[0].len, 
-                         construct_laplacian_matrix(params[0].len), svd_threshold, &ierr);
+  int i = 0;
 
-  params[1].len = 21;
-  params[1].hodlr = allocate_tree_cr(1, &ierr);
+  for (int height = 1; height < 4; height++) {
+    i = 2 * (height - 1);
 
-  double *matrix = construct_laplacian_matrix(params[1].len);
-  matrix[params[1].len - 1] = 0.5;
-  matrix[params[1].len * (params[1].len - 1)] = 0.5;
-  dense_to_tree_hodlr_cr(params[1].hodlr, params[1].len,
-                         matrix, svd_threshold, &ierr);
-  cr_free(matrix);
+    params[i].len = 21;
+    params[i].hodlr = allocate_tree_cr(height, &ierr);
+    dense_to_tree_hodlr_cr(params[i].hodlr, params[i].len, 
+                           construct_laplacian_matrix(params[i].len), 
+                           svd_threshold, &ierr);
 
-  for (int j = 0; j < 2; j++) {
-    params[j].vector = cr_malloc(params[j].len * sizeof(double));
-    for (int i = 0; i < params[j].len; i++) {
-      params[j].vector[i] = 10;
+    params[i+1].len = 21;
+    params[i+1].hodlr = allocate_tree_cr(height, &ierr);
+
+    double *matrix = construct_laplacian_matrix(params[i+1].len);
+    matrix[params[i+1].len - 1] = 0.5;
+    matrix[params[i+1].len * (params[i+1].len - 1)] = 0.5;
+    dense_to_tree_hodlr_cr(params[i+1].hodlr, params[i+1].len,
+                          matrix, svd_threshold, &ierr);
+    cr_free(matrix);
+
+    for (int j = 0; j < 2; j++) {
+      params[i+j].vector = cr_malloc(params[i+j].len * sizeof(double));
+      for (int k = 0; k < params[i+j].len; k++) {
+        params[i+j].vector[k] = 10;
+      }
+      params[i+j].expected = cr_calloc(params[i+j].len, sizeof(double));
     }
-    params[j].expected = cr_calloc(params[j].len, sizeof(double));
+
+    params[i].expected[0] = 10;
+    params[i].expected[params[i].len - 1] = 10;
+
+    params[i+1].expected[0] = 15;
+    params[i+1].expected[params[i+1].len - 1] = 15;
   }
 
-  params[0].expected[0] = 10;
-  params[0].expected[params[0].len - 1] = 10;
-
-  params[1].expected[0] = 15;
-  params[1].expected[params[1].len - 1] = 15;
-  
   return params;
 }
 
