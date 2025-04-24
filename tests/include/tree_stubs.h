777e2eea9917aba3ec6698ebc6f53d458d018624
Rastislav Turanyi <rastislav.turanyi@stfc.ac.uk>
2025-04-24 17:28:05 +0100

    Record results of extract_constructors.py

---
+++ tests/include/tree_stubs.h
@@ -0,0 +1,18 @@
+#ifndef TREE_STUBS_H
+#define TREE_STUBS_H
+
+#include "../../include/tree.h"
+
+int compress_off_diagonal_cr(struct NodeOffDiagonal *, int, int, int, int, double *, double *, double *, double *, double, int *);
+
+int dense_to_tree_hodlr_cr(struct TreeHODLR *, int, double *, double, int *);
+
+void free_partial_tree_hodlr_cr(struct TreeHODLR *, struct HODLRInternalNode **, struct HODLRInternalNode **);
+
+struct TreeHODLR *allocate_tree_cr(const int, int *);
+
+void free_tree_data_cr(struct TreeHODLR *);
+
+void free_tree_hodlr_cr(struct TreeHODLR **);
+
+#endif
