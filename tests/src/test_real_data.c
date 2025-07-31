96c549096b07708b936563caec430f23c1a9554d
Rastislav Turanyi <rastislav.turanyi@stfc.ac.uk>
2025-07-31 17:26:10 +0100

    Add timing information to test_real_data

--- tests/src/test_real_data.c
+++ tests/src/test_real_data.c
@@ -3,6 +3,7 @@
 #endif
 
 #include <math.h>
+#include <time.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -18,7 +19,6 @@
 #include "../include/io.h"
 #include "../include/utils.h"
 #include "../include/common_data.h"
-
 
 struct Parameters {
   int height;
@@ -44,8 +44,11 @@ ParameterizedTestParameters(real_data, H) {
   const int n_params = 1;
   struct Parameters *params = cr_malloc(n_params * sizeof(struct Parameters));
 
-  int m, ierr;
+  int m; clock_t start, end;
+  start = clock();
   double *matrix = read_dense_matrix("data/H", &m, &cr_malloc, &cr_free);
+  end = clock();
+  printf("Matrix read in %f s\n", ((double) (end - start)) / CLOCKS_PER_SEC);
 
   const int heights[] = {4};
   const int *ms[] = {NULL};
@@ -68,6 +71,10 @@ ParameterizedTest(struct Parameters *params, real_data, H) {
   const double svd_threshold = 1e-8, alpha = 1.0, beta = 0.0;
   int ierr = 0; const int m = params->m, inc = 1;
 
+  clock_t start, end;
+  start = clock();
+
+  // Copy matrix
   double *matrix = malloc(matrix_size);
   //memcpy(matrix, params->matrix, matrix_size);
   for (int j = 0; j < m; j++) {
@@ -75,29 +82,46 @@ ParameterizedTest(struct Parameters *params, real_data, H) {
       matrix[i + j * m] = params->matrix[i + j * m];
     }
   }
-  cr_log_info("Matrix copied!");
+  end = clock();
+  cr_log_info("Matrix copied! (in %f s)", 
+              ((double) (end - start)) / CLOCKS_PER_SEC);
 
+  // Set up HODLR
   struct TreeHODLR *hodlr = allocate_tree_monolithic(params->height, &ierr,
                                                      &malloc, &free);
   cr_log_info("HODLR allocated");
   expect_tree_consistent(hodlr, params->height, hodlr->len_work_queue);
+
+  start = clock();
   dense_to_tree_hodlr(hodlr, params->m, params->ms, matrix, 
                       svd_threshold, &ierr, &malloc, &free);
   free(matrix); matrix = NULL;
-  cr_log_info("HODLR constructed!");
+  end = clock();
+  cr_log_info("HODLR constructed! (in %f s)",
+              ((double) (end - start)) / CLOCKS_PER_SEC);
 
+  // Set up vector
   srand(42);
   double *vector = malloc(m * sizeof(double));
   fill_random_matrix(m, 1, vector);
   
+  // Reference vector mult
   double *vector_expected = malloc(m * sizeof(double));
+  start = clock();
   dgemv_("N", &m, &m, &alpha, params->matrix, &m, vector, &inc, 
          &beta, vector_expected, &inc);
-  cr_log_info("Expected vector computed!");
+  end = clock();
+  cr_log_info("Expected vector computed! (in %f s)",
+              ((double) (end - start)) / CLOCKS_PER_SEC);
 
+  // HODLR x vector
+  start = clock();
   double *vector_actual = multiply_vector(hodlr, vector, NULL);
-  cr_log_info("Vector multiplied!");
+  end = clock();
+  cr_log_info("Vector multiplied! (in %f s)",
+              ((double) (end - start)) / CLOCKS_PER_SEC);
 
+  // Check vector operation
   double norm, diff;
   expect_vector_double_eq_custom(
     vector_actual, vector_expected, m, m, 'V', &norm, &diff, 1e-6
@@ -106,15 +130,24 @@ ParameterizedTest(struct Parameters *params, real_data, H) {
   cr_log_info("normv=%f, diff=%f, relerr=%f", sqrtf(norm), sqrtf(diff),
               sqrtf(diff) / sqrtf(norm));
 
+  // Reference matrix mult
   double *matrix_expected = malloc(matrix_size);
+  start = clock();
   dgemm_("N", "N", &m, &m, &m, &alpha, params->matrix, &m, params->matrix, &m,
          &beta, matrix_expected, &m);
-  cr_log_info("Expected matrix computed!");
+  end = clock();
+  cr_log_info("Expected matrix computed! (in %f s)",
+              ((double) (end - start)) / CLOCKS_PER_SEC);
 
+  // HODLR x dense matrix
+  start = clock();
   double *matrix_actual = multiply_hodlr_dense(hodlr, params->matrix, 
                                                m, m, NULL, m);
-  cr_log_info("Matrix multiplied!");
+  end = clock();
+  cr_log_info("Matrix multiplied! (in %f s)",
+              ((double) (end - start)) / CLOCKS_PER_SEC);
 
+  // Check matrix operation
   expect_matrix_double_eq_custom(matrix_actual, matrix_expected, m, m, m, m, 
                                  'M', &norm, &diff, 1e-6);
   cr_log_info("normv=%f, diff=%f, relerr=%f", sqrtf(norm), sqrtf(diff),
