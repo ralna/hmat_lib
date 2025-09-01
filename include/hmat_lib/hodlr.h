b662d1950ac4d1998745801777e1538bebcbf561
Rastislav Turanyi <rastislav.turanyi@stfc.ac.uk>
2025-09-01 16:01:17 +0100

    Extract hodlr_algebra.h
    Extracts hodlr_algebra.c functions from hodlr.h into hodlr_algebra.h

--- include/hmat_lib/hodlr.h
+++ include/hmat_lib/hodlr.h
@@ -262,12 +262,3 @@ struct TreeHODLR {
   struct HODLRLeafNode *memory_leaf_ptr;
 };
 
-
-int multiply_hodlr_hodlr(
-  const struct TreeHODLR *hodlr1,
-  const struct TreeHODLR *hodlr2,
-  struct TreeHODLR *out,
-  const double svd_threshold,
-  int *ierr
-);
-
